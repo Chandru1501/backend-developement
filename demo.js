caeaecadbda7471255ab364385d08fd14762a411
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-23 02:23:39 +0530

    some test codes

--- demo.js
+++ demo.js
@@ -1,1 +1,60 @@
-console.log("hello world");
+// let product = (a,b)=>a*b
+
+// console.log(product(5,5))
+
+// const student = {
+//   name: "chandru",
+//   age: 25,
+//   marks:45,
+//   result : () => {
+//     return student.marks>=40?"pass":"fail"
+//   }
+// }
+
+
+// console.log(student.result())
+
+// let array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+
+// function space(arr){
+//     if(arr==" "){
+//         return "Empty space";
+//     }
+//     else{
+//         return arr;
+//     }
+// }
+
+// console.log(array.map(space));
+
+// const ar = []
+
+// ar.push("hello")
+// console.log(ar)
+
+// const obj1 = {'key1': 1}
+
+// const obj2 = { ...obj1}
+
+// if(obj2 === obj1){
+
+// console.log('same objects')
+
+// }
+
+// else{
+
+// console.log('different objects')
+
+// }
+
+const obj1 = {'key1': 1 , 'key2' : 2}
+
+const obj2 = { ...obj1, key1: 1000}
+
+
+
+console.log(obj1)
+
+console.log(obj2)
+
