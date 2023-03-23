ea975a62a294141c448e405d400b1f80752ba981
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-23 16:15:49 +0530

    learn to creating a server

--- demo.js
+++ demo.js
@@ -1,60 +1,8 @@
-// let product = (a,b)=>a*b
 
-// console.log(product(5,5))
+const http = require("http");
 
-// const student = {
-//   name: "chandru",
-//   age: 25,
-//   marks:45,
-//   result : () => {
-//     return student.marks>=40?"pass":"fail"
-//   }
-// }
+let server = http.createServer((req,res)=>{
+    console.log("CHANDRAPRAKASH");
+})
 
-
-// console.log(student.result())
-
-// let array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
-
-// function space(arr){
-//     if(arr==" "){
-//         return "Empty space";
-//     }
-//     else{
-//         return arr;
-//     }
-// }
-
-// console.log(array.map(space));
-
-// const ar = []
-
-// ar.push("hello")
-// console.log(ar)
-
-// const obj1 = {'key1': 1}
-
-// const obj2 = { ...obj1}
-
-// if(obj2 === obj1){
-
-// console.log('same objects')
-
-// }
-
-// else{
-
-// console.log('different objects')
-
-// }
-
-const obj1 = {'key1': 1 , 'key2' : 2}
-
-const obj2 = { ...obj1, key1: 1000}
-
-
-
-console.log(obj1)
-
-console.log(obj2)
-
+server.listen(4000);
