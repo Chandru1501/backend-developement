a2e070d920e767e6b01df4f37d7eb10a8b1abe42
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-24 03:15:00 +0530

    first small project (redirecting and printing entered value) clearing code into good manner

--- demo.js
+++ demo.js
@@ -1,40 +1,10 @@
 
 const http = require("http");
-const fs = require('fs');
 
-let value="enter a value";
-
-let server = http.createServer((req,res)=>{
-    let url = req.url;
-    let method =req.method
-
-    if(url==="/message" &&  method==="POST"){
-        const body = []
-        req.on("data",(chunk)=>{
-           body.push(chunk);
-           console.log(chunk);
-        })
-        req.on("end",()=>{
-            let parsedbody = Buffer.concat(body).toString();
-            console.log(parsedbody);
-            let message = parsedbody.split("=")
-            console.log(message)
-            fs.writeFileSync('message.txt',message[1])
-            value=message[1];
-        })
-        res.setHeader('Location','/')
-        res.statusCode=302;  // for redirecting
-        return res.end();
-    }
+const requestHandeler = require("./router")
 
 
-    res.setHeader("Content-type","text/html")
-    res.write('<html>')
-    res.write('<head><title>Enter message</title></head>')
-    res.write(`<body><h3>${value}</h3><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></body>`)
-    res.write("</html>")
-    res.end()
-})
+let server = http.createServer(requestHandeler)
 
 
 server.listen(4000);
