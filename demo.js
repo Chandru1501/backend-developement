a4bc697f8b5cfff005c615811efa69dca036928e
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-23 18:02:10 +0530

    learn how to send responses

--- demo.js
+++ demo.js
@@ -2,7 +2,22 @@
 const http = require("http");
 
 let server = http.createServer((req,res)=>{
-    console.log("CHANDRAPRAKASH");
+    let value = ""
+     if(req.url=="/home"){
+          value = "Welcome home"
+    }
+    else if(req.url=="/about"){
+        value = " Welcome to About Us page"
+    }
+    else if(req.url=="/node"){
+        value = " Welcome to my Node Js project"
+    }
+    res.write('<html>')
+    res.write('<head><title>this is my response</title></head>')
+    res.write(`<body><h1>${value}</h1></body>`)
+    res.write("</html>")
+    res.end()
+    process.exit()
 })
 
 server.listen(4000);
