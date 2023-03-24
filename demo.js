a6b6f11d840effe31cc3bd45065c54e59d82cca1
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-24 18:25:20 +0530

    express-js (getting inputs as request and printing in console)

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 
-const http = require("http");
+
 const express = require('express');
+const bodyParser = require("body-parser");
 //const requestHandeler = require("./router")
 
 const app = express();
@@ -8,21 +9,24 @@ const app = express();
 console.log("server running");
 console.log("this server is made by chandraprakash");
 
-app.use((req,res,next)=>{
-    console.log("this is middle ware 1");
-    next();
-})
-app.use((req,res,next)=>{
-    console.log("this is middle ware 2");
-    next();
-})
-app.use((req,res,next)=>{
-    console.log("this is middle ware 3")
-    res.send(`<h1>hello from my express js server</h1>`)
+app.use(bodyParser.urlencoded());
+
+
+app.use("/add-product",(req,res,next)=>{
+    res.send(`<html><head><title>add products</title></head><form action="/storeproduct" method="POST"><h3>Enter product</h3><input type="text" name="product"><h3>Enter size</h3><input type="text" name="size"><button type="submit">Add product</button></html>`)
 })
 
-let server = http.createServer(app);
+app.use("/storeproduct",(req,res,next)=>{
+    console.log("product ",req.body.product)
+    console.log("size ",req.body.size)
+    console.log("product stored");
+    res.send(`<h1>product stored</h1>`)
+})
 
-server.listen(4000);
+app.use("/",(req,res,next)=>{
+    res.send(`<h1>hello</h1>`)
+})
+
+app.listen(4000);
 
 
