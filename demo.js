1aa1eb527965f9e1868440cd1caea70b9045b084
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-24 16:51:43 +0530

    first small project (express js (understanding middle wares))

--- demo.js
+++ demo.js
@@ -1,12 +1,26 @@
 
 const http = require("http");
+const express = require('express');
+//const requestHandeler = require("./router")
 
-const requestHandeler = require("./router")
-
-
-let server = http.createServer(requestHandeler)
+const app = express();
 
 console.log("server running");
-console.log("this server is made by chandraprakash")
+console.log("this server is made by chandraprakash");
+
+app.use((req,res,next)=>{
+    console.log("this is middle ware 1");
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("this is middle ware 2");
+    next();
+})
+app.use((req,res,next)=>{
+    console.log("this is middle ware 3")
+    res.send(`<h1>hello from my express js server</h1>`)
+})
+
+let server = http.createServer(app);
 
 server.listen(4000);
