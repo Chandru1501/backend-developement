dbc0b51c989e4776c49b36a0a86c02494791d093
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-24 14:33:53 +0530

    first small project (redirecting and printing entered value with nodemon)

--- demo.js
+++ demo.js
@@ -6,5 +6,7 @@ const requestHandeler = require("./router")
 
 let server = http.createServer(requestHandeler)
 
+console.log("server running");
+console.log("this server is made by chandraprakash")
 
 server.listen(4000);
